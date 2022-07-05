5d6165999539b2151bd135eae03a27405d64ffe0
sing3demons <sing3demons@live.com>
2022-07-05 18:37:29 +0700

    feat: handle props header menu

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import { AppBar } from '@/constants/index'
+
+type Props = {
+  open: boolean
+  handleDrawerOpen: () => void
+}
+
+export default function Header({ open, handleDrawerOpen }: Props) {
+  return (
+    <AppBar position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
