34cc3105950b0002310959147ccf24040ff5ca88
sing3demons <sing3demons@live.com>
2022-07-05 19:23:03 +0700

    fix: put content into layout

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Typography from '@mui/material/Typography'
 
-type Props = {}
+import Header from '@/components/layouts/Header'
+import Menu from '@/components/layouts/Menu'
+import { DrawerHeader } from '@/constants/index'
 
-export default function Layout({}: Props) {
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  const [open, setOpen] = React.useState(false)
+
   return (
-    <div>Layout</div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <Header open={open} handleDrawerOpen={() => setOpen(true)} />
+      <Menu open={open} handleDrawerClose={() => setOpen(false)} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,92 +1,12 @@
-import * as React from 'react'
-import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles'
-import Box from '@mui/material/Box'
-import MuiDrawer from '@mui/material/Drawer'
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
-import CssBaseline from '@mui/material/CssBaseline'
-import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
-import Header from '../components/layouts/Header'
-import Menu from '../components/layouts/Menu'
+import React from 'react'
+import Layout from '@/components/layouts/Layout'
 
-const drawerWidth = 240
+type Props = {}
 
-const openedMixin = (theme: Theme): CSSObject => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-})
-
-const closedMixin = (theme: Theme): CSSObject => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-})
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}))
-
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}))
-
-export default function Index() {
-  const [open, setOpen] = React.useState(false)
-
+export default function Index({}: Props) {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Header open={open} handleDrawerOpen={() => setOpen(true)} />
-      <Menu open={open} handleDrawerClose={() => setOpen(false)} />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-        <Typography paragraph>content</Typography>
-      </Box>
-    </Box>
+    <Layout>
+      <div>Hello, World</div>
+    </Layout>
   )
 }
